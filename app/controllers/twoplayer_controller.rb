6fe9c78480a66108bd437237dc6fc86c4c5f6e0f
Jonathan Thomas Fung <Jonathanth0mas@yahoo.com>
2015-01-14 17:11:14 -0800

    added two player controller

---
+++ app/controllers/twoplayer_controller.rb
@@ -0,0 +1,2 @@
+class TwoplayerController < ApplicationController
+end
---
+++ test/controllers/twoplayer_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TwoplayerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
